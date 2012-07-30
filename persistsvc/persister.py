36fb67bdb655424c09b9072c2698e1b19e15ec1f
bmullins <bmullins@techresidents.com>
2012-07-30 18:10:24 -0400

    Inserted pseudocode to outline needed functionality.

--- persistsvc/persister.py
+++ persistsvc/persister.py
@@ -44,79 +44,97 @@ class ChatPersisterThreadPool(ThreadPool):
         """
         return self.db_session_factory()
 
-    def process(self, chat_id):
+    def process(self, job_id):
         """Worker thread process method.
 
         This method will be invoked by each worker thread when
-        a new work item (chat_id) is put on the queue.
+        a new work item (job_id) is put on the queue.
         """
-        created = self._create_chat_persist_job(chat_id)
-        if not created:
-            return
-        
         try:
-            self._create_chat_sessions(chat_id)
-            self._end_chat_persist_job(chat_id)
+            self._start_chat_persist_job(job_id)
+            self._create_chat_entities(job_id)
+            self._end_chat_persist_job(job_id)
         except Exception as error:
             self.log.exception(error)
-            self._abort_chat_persist_job(chat_id)
+            self._abort_chat_persist_job(job_id)
 
 
-    def _create_chat_persist_job(self, chat_id, session=None):
-        """Create ChatScheduleJob db record.
+    def _start_chat_persist_job(self, job_id, session=None):
+        """Start ChatPersistJob.
 
-        In order to coordinate across threads, and other instances
-        of this service on other machines, the ChatScheduleJob
-        database table is used. When a thread detects a chat
-        with a closed check_in, and no corresponding ChatScheduleJob
-        record, it will attempt to create one.
-
-        The thread which successfully creates the job record is
-        responsible for processing the item, and must delete
-        the record on exception.
-
-        Returns:
-            True if job created, False otherwise.
+        Mark the current job record as started, by updating the start
+        field with the current timestamp.
         """
-        self.log.info("Attempting to create chat persist job for chat_id=%d ..." % chat_id)
-        result = False
-        try:
-            self.log.info("Created chat persist job for chat_id=%d" % chat_id)
-            result = True
-        except IntegrityError:
-            self.log.warning("Unable to create chat persist job for chat_id=%d, job already exists." % chat_id)
-        except Exception as error:
-            self.log.error("Unable to create chat persist job for chat_id=%d, %s" % (chat_id, str(error)))
-            raise
+        self.log.info("Starting chat persist job for job_id=%d ..." % job_id)
+        session = session or self.create_db_session()
+        job = session.query(ChatPersistJob).filter(ChatPersistJob.job_id==job_id).first()
+        job.start = func.current_timestamp()
+        session.commit()
 
-        return result
-
-    def _end_chat_persist_job(self, chat_id, session=None):
-        """End ChatScheduleJob.
+    def _end_chat_persist_job(self, job_id, session=None):
+        """End ChatPersistJob.
         
         Mark the current job record finished, by updating the end
         field with the current timestamp.
         """
-        self.log.info("Ending chat persist job for chat_id=%d ..." % chat_id)
+        self.log.info("Ending chat persist job for job_id=%d ..." % job_id)
+        session = session or self.create_db_session()
+        job = session.query(ChatPersistJob).filter(ChatPersistJob.job_id==job_id).first()
+        job.end = func.current_timestamp()
+        session.commit()
 
-    def _abort_chat_persist_job(self, chat_id, session=None):
-        """Abort ChatScheduleJob.
+    def _abort_chat_persist_job(self, job_id, session=None):
+        """Abort ChatPersitJob.
 
-        Abort the current schedule job, by deleting the job record.
+        Abort the current persist job, by...
         """
-        self.log.error("Aborting chat persist job for chat_id=%d ..." % chat_id)
+        self.log.error("Aborting chat persist job for job_id=%d ..." % job_id)
+        # TODO I think we should unwind any created minutes and tags and set the job owner as null
 
-    def _create_chat_sessions(self, chat_id, session=None):
-        """Create chat sessions and associated entities.
+    def _create_chat_entities(self, job_id, session=None):
+        """Create chat tags and chat minutes.
 
-        Given a work item (chat_id), this method will create the necessary
-        chat sessions for the chat based on the existing chat registrations
-        with checked_in == True.
-
-        Additionally, it will create the necessary chat users
-        and link existing registrations to their assigned chat sessions.
+        Given a work item (job_id), this method will create the necessary
+        chat tags and minutes for the chat based on the existing chat messages
+        that were created by the chat service.
         """
-        self.log.info("Persisting chat for chat_id=%d" % chat_id)
+        self.log.info("Persisting chat for job_id=%d" % job_id)
+
+        try:
+            session = session or self.create_db_session()
+
+            # Retrieve the chat session id
+            job = session.query(ChatPersistJob).filter(ChatPersistJob.job_id==job_id).first()
+            chat_session_id = job.chat_session.id
+
+            # Retrieve all messages with the specified chat session ID
+            chat_messages = session.query(ChatMessage).\
+                filter(ChatMessage.chat_session == chat_session_id).\
+                all()
+            self.log.info("Found %d messages for chat_session_id=%d" % (len(messages), chat_session_id))
+
+            # Iterate through messages, filtering based upon biz rules
+            chat_messages_to_persist = self._filter_chat_messages(chat_messages)
+
+            # Create chatTags and chatMinutes entries in db
+            for message in chat_messages_to_persist:
+                self.log.info("Persisting messages")
+                # if messageType is X, create db ojbect
+                # if messageType is Y, create db object
+                # Write all message to db
+
+        except Exception:
+            session.rollback()
+            raise
+
+    def _filter_chat_messages(self, chat_messages):
+        """Apply business rules to filter which chat messages will be persisted.
+
+        Given a collection of chat messages associated with a chat, this method will
+        filter the messages down to only those we want to persist.
+        """
+        self.log.info("Filtering chat messages based upon business rules")
+
 
 class ChatPersister(object):
     """Chat Persister creates and delegates work items to the ChatPersisterThreadPool.
@@ -133,7 +151,7 @@ class ChatPersister(object):
         self.log = logging.getLogger(__name__)
         self.num_threads = num_threads
         self.db_session_factory = db_session_factory
-        self.poll_seconds = poll_seconds
+        self.poll_seconds = poll_seconds #TODO remove
         self.monitorThread = threading.Thread(target=self.run)
         self.threadpool = ChatPersisterThreadPool(num_threads, db_session_factory)
 
@@ -163,13 +181,23 @@ class ChatPersister(object):
 
         while self.running:
             try:
-                #Look for chats with closed check_in and no
-                #existings ChatScheduleJob record.
-                #This combination indicates a chat which
-                #needs to be processed.
                 self.log.info("ChatPersister is running")
 
+                #Look for ChatPersistJobs with no owner.
+                #This indicates a job which
+                #needs to be processed.
+                for job in session.query(ChatPersistJob).\
+                        filter(ChatPersistJob.owner == None):
+
+                    #delegate chats to threadpool for processing
+                    self.threadpool.put(job.id)
+
+                #commit is required so changes to db will be
+                #reflected (MVCC).
+                session.commit()
+
             except Exception as error:
+                session.rollback()
                 self.log.exception(error)
             
             #Acquire exit conditional variable
